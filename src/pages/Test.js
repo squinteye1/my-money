fb7991e12981b3327d30a42cc767a7e23418ae01
squinteye1 <neil.d.mckay@googlemail.com>
2022-02-03 23:53:51 +0000

    first commit

---
+++ src/pages/Test.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default function Test() {
+  return (
+  <div>
+      <h1>Test Page</h1>
+  </div>);
+}
---
+++ src/pages/home/Home.js
@@ -0,0 +1,10 @@
+import styles from './Home.module.css';
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div>
+        Home
+    </div>
+  );
+}
